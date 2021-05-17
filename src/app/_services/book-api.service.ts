ef143b9fe78d8c31145723b6599e500892b054af
Jason <jason@angleorange.com>
2021-05-17 19:53:26 +0200

    feat: using azure function as base url

--- src/app/_services/book-api.service.ts
+++ src/app/_services/book-api.service.ts
@@ -7,22 +7,25 @@ import { Observable } from 'rxjs';
 })
 export class BookApiService {
 
-  protected baseUri = 'http://openlibrary.org';
+  protected baseUri = ' http://localhost:7071/api';
 
   constructor(private http: HttpClient) { }
 
   searchBooks(query: string): Observable<any> {
-    const searchBooks = `${this.baseUri}/search.json?q=${query.replace(' ', '+')}/`;
+    // const searchBooks = `${this.baseUri}/search.json?q=${query.replace(' ', '+')}/`;
+    const searchBooks = `${this.baseUri}/search?q=${query}/`;
     return this.http.get(searchBooks);
   }
 
   getBookDetailsByIsbn(isbn: string): Observable<any> {
-    const bookByIsbn = `${this.baseUri}/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=details`;
+    //const bookByIsbn = `${this.baseUri}/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=details`;
+    const bookByIsbn = `${this.baseUri}/bookdetail?isbn${isbn}`;
     return this.http.get(bookByIsbn);
   }
 
   getBookDetailsByWork(work: string): Observable<any> {
-    const bookByWork = `${this.baseUri}${work}.json`;
+    //const bookByWork = `${this.baseUri}${work}.json`;
+    const bookByWork = `${this.baseUri}?work=${work}`;
     return this.http.get(bookByWork);
   }
 }
