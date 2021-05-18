bfc2325a9ffa0e02adcc6a05d50899f0b88134c0
Jason <jason@angleorange.com>
2021-05-18 07:50:51 +0200

    feat: added azure function endpoint

--- src/app/_services/book-api.service.ts
+++ src/app/_services/book-api.service.ts
@@ -12,20 +12,18 @@ export class BookApiService {
   constructor(private http: HttpClient) { }
 
   searchBooks(query: string): Observable<any> {
-    // const searchBooks = `${this.baseUri}/search.json?q=${query.replace(' ', '+')}/`;
     const searchBooks = `${this.baseUri}/search?q=${query}/`;
     return this.http.get(searchBooks);
   }
 
   getBookDetailsByIsbn(isbn: string): Observable<any> {
-    //const bookByIsbn = `${this.baseUri}/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=details`;
-    const bookByIsbn = `${this.baseUri}/bookdetail?isbn${isbn}`;
+    console.log(isbn);
+    const bookByIsbn = `${this.baseUri}/bookdetail?isbn=${isbn}`;
     return this.http.get(bookByIsbn);
   }
 
   getBookDetailsByWork(work: string): Observable<any> {
-    //const bookByWork = `${this.baseUri}${work}.json`;
-    const bookByWork = `${this.baseUri}?work=${work}`;
+    const bookByWork = `${this.baseUri}/bookwork?work=${work.slice(work.lastIndexOf('/') + 1)}`;
     return this.http.get(bookByWork);
   }
 }
